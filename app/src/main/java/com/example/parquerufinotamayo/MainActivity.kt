af7432c84454a4d19ffddd0b982bec10e9f3e89f
Luis <luiseduardo.gtz@outlook.com>
2021-10-05 11:14:18 -0500

    BottomTabNavigation with Fragments

--- app/src/main/java/com/example/parquerufinotamayo/MainActivity.kt
+++ app/src/main/java/com/example/parquerufinotamayo/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.parquerufinotamayo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.parquerufinotamayo.Fragments.HomeFragment
+import com.example.parquerufinotamayo.Fragments.NewReportFragment
+import com.example.parquerufinotamayo.Fragments.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomTabListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when (it.itemId) {
+            R.id.action_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.flContainer, HomeFragment()).commit();
+                return@OnNavigationItemSelectedListener true;
+            }
+            R.id.action_user -> {
+                supportFragmentManager.beginTransaction().replace(R.id.flContainer, UserFragment()).commit();
+                return@OnNavigationItemSelectedListener true;
+            }
+            R.id.action_new_report -> {
+                supportFragmentManager.beginTransaction().replace(R.id.flContainer, NewReportFragment()).commit();
+                return@OnNavigationItemSelectedListener true;
+            }
+        }
+        false;
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Define bottom tab navigation and default fragment (Home)
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.selectedItemId = R.id.action_home;
+        supportFragmentManager.beginTransaction().replace(R.id.flContainer, HomeFragment()).commit();
+
+        //Navigation item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomTabListener);
+
     }
+
+
 }
